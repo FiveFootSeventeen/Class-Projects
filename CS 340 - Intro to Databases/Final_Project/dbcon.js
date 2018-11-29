75669ae2f184e1d4b036c61ae34ca7e108498ddc
Jacob Leno <lenoja@oregonstate.edu>
2018-11-28 17:14:22 -0700

    OSU CS Projects

--- CS 340 - Intro to Databases/Final_Project/dbcon.js
+++ CS 340 - Intro to Databases/Final_Project/dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_lenoja',
-  password        : '1376',
+  password        : 'XXXX',
   database        : 'cs340_lenoja'
 });
 module.exports.pool = pool;
